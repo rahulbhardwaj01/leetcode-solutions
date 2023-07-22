07e533f10bb9e7b1021cedf416cfb197191fa19d
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2023-07-22 23:28:34 +0530

    Time: 4 ms (44.01%), Space: 6.5 MB (27.71%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-            int c=0;
+        int c=0;
         for(int i=0;i<s.size();i++){
             if(s[i]==' '){
                 c++;
