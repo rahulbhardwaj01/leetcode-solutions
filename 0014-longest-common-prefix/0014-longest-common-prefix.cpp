768b59ad2858bc20951f47bc1701335299bde3f0
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2023-07-23 04:05:35 +0530

    Time: 4 ms (77.96%), Space: 9.1 MB (62.68%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,5 +1,9 @@
 // a bit more timecomplexity other solution used sorting and then compared first and last index strings bcs acc to them lexographical order is followed bby strings::
 
+
+// sort() function implicitly converts strings into ASCII while sorting.
+// like in inetegrs it sort similiarly sees strings as ascii and sorts all"""
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs)
@@ -19,3 +23,30 @@ public:
         return ans;
     }
 };
+
+// 
+
+// class Solution {
+//  public:
+//      string longestCommonPrefix(vector<string>& strs){
+
+//          sort(strs.begin(),strs.end());
+//          int i=0;
+//          int j=0;
+//          string s1=strs[0];
+//          string s2=strs[strs.size()-1];
+//          string ans="";
+//          while(i<s1.size()&& j<s2.size()){
+//              if(s1[i]==s2[j]){
+//                  ans+=s1[i];
+//              }
+//              else{
+//                  break;
+//              }
+//          }
+//          return ans;
+
+//      }
+// };
+
+// tle bcs tc==0(nlogn*m) for strings
