f52983f78ca34310345d872187400d24e3d2142b
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2023-07-27 23:28:01 +0530

    Time: 59 ms (53.19%), Space: 41.7 MB (68.75%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -5,3 +5,7 @@ var argumentsLength = function(...args) {
  * argumentsLength(1, 2, 3); // 3
  */
 
+
+// In JavaScript, arguments are passed as an array-like object. Because of this, you can use a lot of the same kind of functions on arguments as you can on arrays, including using args.length.
+
+// And so, in the same way you would find the number of elements in an array arr with arr.length, you can find the number of arguments in args with args.length.
