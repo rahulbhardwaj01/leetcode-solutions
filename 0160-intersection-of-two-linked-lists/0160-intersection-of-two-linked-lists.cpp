0c87683e25a9fe270c7bf05c9e413f38a87ea4fe
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2023-07-30 23:50:20 +0530

    Time: 42 ms (95.93%), Space: 14.4 MB (99.20%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,3 +1,4 @@
+// two pointer solution::
 class Solution {
 public:
 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
@@ -20,3 +21,88 @@ public:
 		return ptr1;
 	}
 };
+
+// 1. Brute Force Solution
+// Time Complexity : O(m * n)
+// Space Complexity : O(1)
+
+// class Solution {
+// public:
+// 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// 		ListNode *temp;
+// 		while(headA != NULL){
+// 			temp = headB;
+// 			while(temp != NULL){
+// 				if(headA == temp){
+// 					return headA;
+// 				}
+// 				temp = temp -> next;
+// 			}
+// 			headA = headA -> next;
+// 		}
+// 		return NULL;
+// 	}
+// };
+// 2. HashMap Solution
+// Time Complexity : O(m + n)
+// Space Complexity : O(m)
+
+// class Solution {
+// public:
+// 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// 		unordered_map<ListNode*, int> m;
+// 		while(headA != NULL){
+// 			m[headA]++;
+// 			headA = headA -> next;
+// 		}
+// 		while(headB != NULL){
+// 			if(m[headB] > 0){
+// 				return headB;
+// 			}
+// 			headB = headB -> next;
+// 		}
+// 		return NULL;
+// 	}
+// };
+// 3. Length Difference Solution
+// Time Complexity : O(m + n)
+// Space Complexity : O(1)
+
+// class Solution {
+// public:
+// 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// 		int n = 0;
+// 		int m = 0;
+// 		ListNode* ptr1 = headA;
+// 		ListNode* ptr2 = headB;
+// 		while(ptr1 != NULL){
+// 			n++;
+// 			ptr1 = ptr1 -> next;
+// 		}
+// 		while(ptr2 != NULL){
+// 			m++;
+// 			ptr2 = ptr2 -> next;
+// 		}
+// 		int t = abs(n - m);
+// 		if(n > m){
+// 			while(t){
+// 				headA = headA -> next;
+// 				t--;
+// 			}
+// 		}
+// 		else{
+// 			while(t){
+// 				headB = headB -> next;
+// 				t--;
+// 			}
+// 		}
+// 		while(headA != NULL and headB != NULL){
+// 			if(headA == headB){
+// 				return headA;
+// 			}
+// 			headA = headA -> next;
+// 			headB = headB -> next;
+// 		}
+// 		return NULL;
+// 	}
+// };
