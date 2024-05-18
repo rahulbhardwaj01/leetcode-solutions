31ce914504809a4d525f70034ed8a37a143e87af
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2024-05-19 00:39:50 +0530

    Time: 2 ms (64.97%), Space: 13.5 MB (35.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,64 @@
 class Solution {
-    int findinv(vector<int>& nums){
-        int n = nums.size(),lo=0, hi= n-1, invIdx=0;
-        while(lo <= hi){
-            int mid = (hi+lo)>>1;
-            if(mid != 0 && nums[mid] < nums[mid-1]){
-                invIdx = mid;
-                break;
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            if(nums[mid] < nums[0]) hi = mid-1;
-            else lo = mid+1;
-        }
-        return invIdx;
-    }
+            // first check sorted half;
+            if(nums[low]<=nums[mid]){
+            // now check for sprted part bcs it would be very easwy to checlk there;
+                if(nums[low]<=target && target<nums[mid]){
+                    high=mid-1;
+                } 
+                else{
+                    low=mid+1;
 
-    int bs(vector<int>& nums, int lo, int hi, int key){
-        while(lo <= hi){
-            int mid= (hi+lo)>>1;
-            if(nums[mid] == key) return mid;
-            if(nums[mid] < key) lo = mid+1;
-            else hi = mid-1;
+                }
+            }
+            else{
+                if(nums[mid]<target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
         }
         return -1;
     }
-public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size(), invIdx=0;
-        if(nums[n-1] < nums[0]) invIdx = findinv(nums);
-        int ans = bs(nums, 0, invIdx-1, target);
-        if(ans == -1) ans = bs(nums, invIdx, n-1, target);
-        return ans;
-    }
 };
+
+// class Solution {
+// public:
+//     int search(std::vector<int>& nums, int target) {
+//         int low = 0, high = nums.size() - 1;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
+
+//             if (nums[low] <= nums[mid]) {
+//                 if (nums[low] <= target && target < nums[mid]) {
+//                     high = mid - 1;
+//                 } else {
+//                     low = mid + 1;
+//                 }
+//             } else {
+//                 if (nums[mid] < target && target <= nums[high]) {
+//                     low = mid + 1;
+//                 } else {
+//                     high = mid - 1;
+//                 }
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
