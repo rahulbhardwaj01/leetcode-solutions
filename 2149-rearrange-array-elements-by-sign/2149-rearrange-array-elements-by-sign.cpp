3d7af05e5945cba88526351e481288c736b555b2
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2024-05-20 00:37:36 +0530

    Time: 148 ms (40.88%), Space: 143.1 MB (13.40%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -26,3 +26,20 @@ public:
     }
     
 };
+// more optimal----->>>
+// vector<int> rearrangeArray(vector<int>& nums) {
+//         vector<int> ans(n,0);
+//         int indexpos = 0, indexneg=1;
+//         for(auto num: nums){
+//             if(num>0){
+//                 ans[indexpos] = num;
+//                 indexpos+=2;
+//             }
+//             if(num<0){
+//                 ans[indexneg] = num;
+//                 indexneg += 2;
+//             }
+//         }
+//         return ans;
+        
+//     }
