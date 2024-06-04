6ac19fa6974c7f9fbe6069147eaffd0f4f4904b7
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2024-06-04 13:03:41 +0530

    Time: 6 ms (30.54%), Space: 11.2 MB (85.68%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -13,9 +13,7 @@ public:
             if((mid==0 && nums[mid]>nums[mid+1]) || (mid==n-1 &&  nums[mid]>nums[mid-1] )){
                 return mid;
             }
-            // if(mid==n-1 &&  nums[mid]>nums[mid-1]){
-            //     return mid;
-            // }
+            
             if(nums[mid]>nums[mid+1]&& nums[mid]>nums[mid-1]){
                 return mid;
             }
