8e7ab551c715d9dfbe0c971abf0cc1a34bcd4b2e
Rahul <128632923+rahulbhardwaj01@users.noreply.github.com>
2024-06-06 22:39:40 +0530

    Time: 5 ms (21.22%), Space: 12.5 MB (59.18%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low=0;
-        int high=nums.size()-1;
-//         we have to find min for every sortwd subparts
+        int n=nums.size();
+        int l=0;
+        int h=n-1;
         int ans=INT_MAX;
-        // apply binary search
-        while(low<=high){
-            int mid=(low+high)/2;
-            // see for sorted part and find min index using low{bcs it is min in sorted arrayu}
-            if(nums[low]<=nums[mid] ){
-                ans=min(ans,nums[low]);
-                low=mid+1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            // sorted lefttt
+            if(nums[mid]>=nums[l]){
+                ans=min(ans,nums[l]);
+                l=mid+1;
             }
             else{
                 ans=min(ans,nums[mid]);
-                high=mid-1;
-
+                h=mid-1;
             }
-
-        }return ans;
+        }
+        return ans;
     }
 };
